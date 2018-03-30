f00ee113d00272e898408bf307b402b0b35a6985
Michael Pichugin <michaelpichugin@yandex.ru>
2018-03-30 19:37:35 +0700

    DataGridView Export Excel

---
+++ ExportDataGridView/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ExportDataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(620, 179);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 197);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(539, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(557, 197);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "&Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(266, 223);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(74, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Процесс...0%";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 247);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label lblStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ ExportDataGridView/Form1.cs
@@ -0,0 +1,174 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExportDataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct DataParameter
+        {
+            public List<String> oneColumn;
+            public List<String> twoColumn;
+            public List<String> threeColumn;
+            public String FileName { get; set; }
+        }
+
+        DataParameter _inputParameter;
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<String> one = ((DataParameter)e.Argument).oneColumn;
+            List<String> two = ((DataParameter)e.Argument).twoColumn;
+            List<String> three = ((DataParameter)e.Argument).threeColumn;
+            String FileName = ((DataParameter)e.Argument).FileName;
+
+            Excel.Application excel = new Excel.Application();
+            Excel.Workbook wb = excel.Workbooks.Add(Excel.XlSheetType.xlWorksheet);
+            Excel.Worksheet ws = (Excel.Worksheet)excel.ActiveSheet;
+            excel.Visible = false;
+            int index = 1;
+            int progress = one.Count;
+            //Колонки
+            ws.Cells[1, 1] = "Первый)";
+            ws.Cells[1, 2] = "Второй=)";
+            ws.Cells[1, 3] = "Третий";
+
+            /*
+            ((Excel.Range)sheet.Columns).ColumnWidth = 15;
+            
+            //жирность
+            (sheet.Cells[1, 1] as Excel.Range).Font.Bold = true;
+            
+            //размер шрифта
+            (sheet.Cells[1, 1] as Excel.Range).Font.Size = 16;
+            
+            //название шрифта
+            (sheet.Cells[1, 1] as Excel.Range).Font.Name = "Times New Roman";
+            
+            //стиль границы
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle = Excel.XlLineStyle.xlContinuous;
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle = Excel.XlLineStyle.xlContinuous;
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle = Excel.XlLineStyle.xlContinuous;
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle = Excel.XlLineStyle.xlContinuous;
+            
+            //толщина границы
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeTop].Weight = Excel.XlBorderWeight.xlMedium;
+            
+            //выравнивание по горизонтали
+            (ws.Cells[1, 1] as Excel.Range).HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            
+            //выравнивание по вертикали
+            (ws.Cells[1, 1] as Excel.Range).VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            
+            //объединение ячеек
+            Excel.Range oRange;
+            oRange = ws.Range[ws.Cells[4, 2], ws.Cells[9, 5 + 4]];
+            oRange.Merge(Type.Missing);
+            */
+
+            //Жирность
+            (ws.Cells[1, 1] as Excel.Range).Font.Bold = true;
+            (ws.Cells[1, 2] as Excel.Range).Font.Bold = true;
+            (ws.Cells[1, 3] as Excel.Range).Font.Bold = true;
+
+            Excel.Range oRange;
+            oRange = ws.Range[ws.Cells[4, 2], ws.Cells[9, 5 + 4]];
+            oRange.Merge(Type.Missing);
+            (ws.Cells[4, 2] as Excel.Range).HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            (ws.Cells[4, 2] as Excel.Range).VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            (ws.Cells[4, 2] as Excel.Range).Font.Size = 26;
+            (ws.Cells[4, 2] as Excel.Range).Font.Name = "Times New Roman";
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle = Excel.XlLineStyle.xlContinuous;
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle = Excel.XlLineStyle.xlContinuous;
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle = Excel.XlLineStyle.xlContinuous;
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle = Excel.XlLineStyle.xlContinuous;
+            ws.get_Range("B2", "C3").Borders[Excel.XlBordersIndex.xlEdgeTop].Weight = Excel.XlBorderWeight.xlMedium;
+            
+            for (int i = 0; i < one.Count; i++)
+            {
+                if (!backgroundWorker1.CancellationPending)
+                {
+                    backgroundWorker1.ReportProgress(index++ * 100 / progress);
+                    ws.Cells[index, 1] = one[i];
+                    ws.Cells[index, 2] = two[i];
+                    ws.Cells[index, 3] = three[i];
+                }
+            }
+
+            ws.Columns[1].AutoFit();
+            ws.Columns[2].AutoFit();
+            ws.Columns[3].AutoFit();
+            //Сохранение файла
+            ws.SaveAs(FileName, Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, Excel.XlSaveAsAccessMode.xlNoChange, Excel.XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+            excel.Quit();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblStatus.Text = String.Format("Процесс выполнения...{0}%", e.ProgressPercentage);
+            progressBar1.Update();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                Thread.Sleep(100);
+                lblStatus.Text = "Ваши данные успешно экспортированы.";
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy)
+                return;
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xls" })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    _inputParameter.FileName = sfd.FileName;
+                    _inputParameter.oneColumn = new List<string>();
+                    _inputParameter.twoColumn = new List<string>();
+                    _inputParameter.threeColumn = new List<string>();
+
+                    for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[0].Value != null)
+                            _inputParameter.oneColumn.Insert(i, dataGridView1.Rows[i].Cells[0].Value.ToString());
+                        else
+                            _inputParameter.oneColumn.Insert(i, "");
+
+                        if (dataGridView1.Rows[i].Cells[1].Value != null)
+                            _inputParameter.twoColumn.Insert(i, dataGridView1.Rows[i].Cells[1].Value.ToString());
+                        else
+                            _inputParameter.twoColumn.Insert(i, "");
+
+                        if (dataGridView1.Rows[i].Cells[2].Value != null)
+                            _inputParameter.threeColumn.Insert(i, dataGridView1.Rows[i].Cells[2].Value.ToString());
+                        else
+                            _inputParameter.threeColumn.Insert(i, "");
+                    }
+                    progressBar1.Minimum = 0;
+                    progressBar1.Value = 0;
+                    backgroundWorker1.RunWorkerAsync(_inputParameter);
+                }
+            }
+        }
+    }
+}
